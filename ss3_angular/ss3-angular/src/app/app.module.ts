6ccdf2a640cc03e54a3db0beee1b5ed1e7d643a0
Trần Hoàng Long <01662121970ax@gmail.com>
2022-09-23 14:09:12 +0700

    ss4

--- ss3_angular/ss3-angular/src/app/app.module.ts
+++ ss3_angular/ss3-angular/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { CalculatorComponent } from './calculator/calculator.component';
 import { ColorPickerComponent } from './color-picker/color-picker.component';
 import { NewsComponent } from './news/news.component';
 import { LikeComponent } from './news/like/like.component';
+import { NavBarComponent } from './news/nav-bar/nav-bar.component';
+import { FooterComponent } from './news/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +20,9 @@ import { LikeComponent } from './news/like/like.component';
     CalculatorComponent,
     ColorPickerComponent,
     NewsComponent,
-    LikeComponent
+    LikeComponent,
+    NavBarComponent,
+    FooterComponent
   ],
     imports: [
         BrowserModule,
